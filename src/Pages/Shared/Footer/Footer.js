f0ad865515751e927290d8cf8b57c7577699a2cc
rony9669 <ronychowdhury.cse@gmail.com>
2022-12-22 00:27:45 +0600

    full website

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.css";
+import { FaFacebook, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const ContactUs = () => {
   return (
@@ -8,8 +9,14 @@ const ContactUs = () => {
         <div className="container">
           <div className="row">
             <div className="copyright-text ">
-              <p className=" text-center">
-                Copyright &copy; 2022, All Right Reserved
+              <p className=" text-center ">
+                <div className="d-flex justify-content-center align-items-center mb-1 gap-2 ">
+                  <FaFacebook />
+                  <FaLinkedin />
+                  <FaTwitter />
+                  <FaYoutube />
+                </div>
+                <div>Copyright &copy; 2022, All Right Reserved</div>
               </p>
             </div>
           </div>
